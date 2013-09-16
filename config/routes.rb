e64c431f0466e4806ea03884c3c9211ff26f2718
Scott Fey <scottfey@gmail.com>
2013-09-16 13:43:55 -0500

    Remove Devise + Tenant Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 GreatTenantApp::Application.routes.draw do
-  devise_for :tenants
   root to: "static_pages#home"
   get "static_pages/home"
-  resources :tenants
+  get "static_pages/test_page"
 end
