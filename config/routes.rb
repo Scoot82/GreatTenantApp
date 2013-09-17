3967d8d5b6990651f267027a89c321eb88b7ae88
Scott Fey <scottfey@gmail.com>
2013-09-17 10:34:39 -0500

    Make basic Tenant model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 GreatTenantApp::Application.routes.draw do
+  get "tenants/new"
   root to: "static_pages#home"
   get "static_pages/home"
   get "static_pages/test_page"
---
+++ db/migrate/20130917152828_add_password_digest_to_tenants.rb
@@ -0,0 +1,5 @@
+class AddPasswordDigestToTenants < ActiveRecord::Migration
+  def change
+    add_column :tenants, :password_digest, :string
+  end
+end
